17f6d5ea9bc56b5476f5e88b99735ab5e0bd7934
lortisen <jarl.silven@gmail.com>
2016-11-01 16:39:49 +0100

    :wrench: Improved proof structure and readability, added get_box for searching

--- bvk.pl
+++ bvk.pl
@@ -5,67 +5,58 @@ verify(InputFileName) :- see(InputFileName),
 	valid_proof(Prems, Goal, Proof).
 
 % kontrollera att ett logiskt bevis är syntaktiskt korrekt.
-valid_proof(_,_,[[_,_,K|_]|_]) :- K = assumption,fail,!.
+valid_proof(_,_,[[_,_,assumption]|_]) :- fail,!.
 valid_proof(Prems, Goal, Proof) :-
 	reverse(Proof,Foorp),
-	Foorp = [[_,Result|_]|_],
-	Goal = Result,
-	prove(Prems,Foorp,[]).
+	Foorp = [[_,Goal|_]|_],
+	prove(Prems,Foorp,Foorp).
 
-% bevisa en rad, P = premisser, F = bakbevis, E = tidigare rader, K = konnektiv.
-prove(_,[],_) :- !.
-prove(P,F,E) :- F = [[_,_,K]|T], K = premise, premise(F,P), prove(P,T,E),!.
-prove(P,F,E) :- F = [[_,_,K]|T], call(K,F,E), prove(P,T,E),!.
+% bevisa en rad, P = premisser, F = bakbevis, C = nuvarande box, K = konnektiv.
+prove(_,_,[]) :- !.
+prove(P,[_|FT],C) :- C = [[_,S,K]|CT], K = premise,!,premise(P,S),prove(P,FT,CT).
+prove(P,[_|FT],C) :- C = [[_,_,K]|CT], K = assumption,!,assumption(C),prove(P,FT,CT).
+prove(P,F,[_|CT]) :- F = [[_,R,K]|FT], call(K,F,R),prove(P,FT,CT),!.
 % nästa rad är box. B = box, X = xob, Y = Förra boxens rest och tidigare rader.
-prove(P,[B|T],E) :- reverse(B,X), append(T,E,Y), prove(P,X,Y), prove(P,T,E).
+prove(P,[B|F],[B|C]) :- reverse(B,X), append(X,F,Y), prove(P,Y,X), prove(P,F,C).
 
 % introducerar rader från premisser, S = sekvens, P = premisser.
-premise([[_,S|_]|_],P) :- in_list(S,P).
+premise(P,S) :-
+	in_list(S,P).
 
 % antagande, bara möjligt i början av en box, T = bevissvans.
-assumption([_|T],_) :- T = [].
+assumption([_|T]) :-
+	T = [].
 
-% kopiera en tidigare rad, I = index, R = resultat, V = värde
-copy(I,F,E) :-
-	F = [[_,R|_]|_],
-	get_seq(I,F,E,V),
+% kopiera en tidigare rad, I = index, P = bevis, R = resultat, V = värde
+copy(I,P,R) :-
+	get_seq(I,P,V),
 	R = V.
 
-% introducerar implikation, Bi = begin, Ei = end, A = assumption, C = konsekvens
-impint(Bi,Ei,F,_) :-
-	F = [[_,R|_],B|_],
-	B = [[_,A|_]|_],
-	check_box(B,[[_,C|_]|_],Bi,Ei),
+% introducerar implikation, Bi = begin, Ei = end, A = antagande, C = konsekvens
+impint(Bi,Ei,P,R) :-
+	get_box(Bi,Ei,P,A,C),
 	R = imp(A,C).
 
-% tar bort implikation, Vi = värdeindex, Ii = implikationindex, F = bakbevis.
-impel(Vi,Ii,F,E) :-
-	F = [[_,R|_]|_],
-	get_seq(Vi,F,E,V),
-	get_seq(Ii,F,E,I),
+% tar bort implikation, Vi = värdets index, Ii = implikationens index, P = bevis.
+impel(Vi,Ii,P,R) :-
+	get_seq(Vi,P,V),
+	get_seq(Ii,P,I),
 	I = imp(V,R).
 
-% introducerar dubbelnegation, Vi = värdets index, F = bakbevis, R = resultat
-negnegint(Vi,F,E) :- 
-	F = [[_,R|_]|_],
-	get_seq(Vi,F,E,V),
+% introducerar dubbelnegation, Vi = värdets index, P = bevis, R = resultat
+negnegint(Vi,P,R) :-
+	get_seq(Vi,P,V),
 	R = neg(neg(V)).
 
-get_seq(I,F,_,S) :- get_seqh(I,F,S),!.
-get_seq(I,_,E,S) :- get_seqh(I,E,S).
 % hämtar sekvens mhja index, I = index, H = huvud, T = bevissvans S = sekvens.
-get_seqh(_,[],_) :- fail.
-get_seqh(I,[[H,S|_]|_],S) :- I = H,!.
-get_seqh(I,[_|T],S) :- get_seqh(I,T,S).
+get_seq(_,[],_) :- fail.
+get_seq(I,[[H,S,_]|_],S) :- I = H,!.
+get_seq(I,[_|T],S) :- get_seq(I,T,S).
 
-% se om box har rätt egenskaper, B = box, X = xob, Bi = begin, Ei = end.
-check_box(B,X,Bi,Ei) :-
-	reverse(B,X),
-	B = [[Pi,_,A]|_],
-	X = [[Fi|_]|_],
-	Pi = Bi,
-	Fi = Ei,
-	A = assumption.
+% hitta rätt box, Bi = begin, Ei = end, P = bevis, A = antagande, C = resultat
+get_box(_,_,[],_,_) :- fail.
+get_box(Bi,Ei,[B|_],A,C) :- B = [[Bi,A,assumption]|_],reverse(B,[[Ei,C,_]|_]),!.
+get_box(Bi,Ei,[_|T],A,C) :- get_box(Bi,Ei,T,A,C).
 
 % kolla om element finns i lista ( därför att member förstör tester.. )
 in_list(_,[]) :- fail.
