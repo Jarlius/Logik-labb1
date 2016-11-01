99db453f6dcd27cff5a89bf277b405c838ab21b8
lortisen <jarl.silven@gmail.com>
2016-11-01 16:46:17 +0100

    :book: Changed documentation from swedish to english.

--- bvk.pl
+++ bvk.pl
@@ -1,64 +1,64 @@
-% verifiera att filen innehåller ett korrekt bevis.
+% verify that the file contains a valid proof
 verify(InputFileName) :- see(InputFileName),
 	read(Prems), read(Goal), read(Proof),
 	seen,
 	valid_proof(Prems, Goal, Proof).
 
-% kontrollera att ett logiskt bevis är syntaktiskt korrekt.
+% make sure a logical proof is syntactically correct
 valid_proof(_,_,[[_,_,assumption]|_]) :- fail,!.
 valid_proof(Prems, Goal, Proof) :-
 	reverse(Proof,Foorp),
 	Foorp = [[_,Goal|_]|_],
 	prove(Prems,Foorp,Foorp).
 
-% bevisa en rad, P = premisser, F = bakbevis, C = nuvarande box, K = konnektiv.
+% prove one row, P = premises, F = foorp, C = current box, K = connective
 prove(_,_,[]) :- !.
 prove(P,[_|FT],C) :- C = [[_,S,K]|CT], K = premise,!,premise(P,S),prove(P,FT,CT).
 prove(P,[_|FT],C) :- C = [[_,_,K]|CT], K = assumption,!,assumption(C),prove(P,FT,CT).
 prove(P,F,[_|CT]) :- F = [[_,R,K]|FT], call(K,F,R),prove(P,FT,CT),!.
-% nästa rad är box. B = box, X = xob, Y = Förra boxens rest och tidigare rader.
+% next row must be a box, B = box, X = xob, Y = rest of proof including box.
 prove(P,[B|F],[B|C]) :- reverse(B,X), append(X,F,Y), prove(P,Y,X), prove(P,F,C).
 
-% introducerar rader från premisser, S = sekvens, P = premisser.
+% introducing rows from premises, S = sequence, P = premises.
 premise(P,S) :-
 	in_list(S,P).
 
-% antagande, bara möjligt i början av en box, T = bevissvans.
+% assumption, only possible at the start of a box, T = tail of proof.
 assumption([_|T]) :-
 	T = [].
 
-% kopiera en tidigare rad, I = index, P = bevis, R = resultat, V = värde
+% copy a previous row, I = index, P = proof, R = result, V = value
 copy(I,P,R) :-
 	get_seq(I,P,V),
 	R = V.
 
-% introducerar implikation, Bi = begin, Ei = end, A = antagande, C = konsekvens
+% implication, Bi = begin, Ei = end, A = assumption, C = consequence
 impint(Bi,Ei,P,R) :-
 	get_box(Bi,Ei,P,A,C),
 	R = imp(A,C).
 
-% tar bort implikation, Vi = värdets index, Ii = implikationens index, P = bevis.
+% removes implication, Vi = value index, Ii = implication index, P = proof.
 impel(Vi,Ii,P,R) :-
 	get_seq(Vi,P,V),
 	get_seq(Ii,P,I),
 	I = imp(V,R).
 
-% introducerar dubbelnegation, Vi = värdets index, P = bevis, R = resultat
+% introduces doublenegation, Vi = value index, P = proof, R = result
 negnegint(Vi,P,R) :-
 	get_seq(Vi,P,V),
 	R = neg(neg(V)).
 
-% hämtar sekvens mhja index, I = index, H = huvud, T = bevissvans S = sekvens.
+% find sequence at index, I = index, H = head, T = tail S = sequence.
 get_seq(_,[],_) :- fail.
 get_seq(I,[[H,S,_]|_],S) :- I = H,!.
 get_seq(I,[_|T],S) :- get_seq(I,T,S).
 
-% hitta rätt box, Bi = begin, Ei = end, P = bevis, A = antagande, C = resultat
+% find correct box, Bi = begin, Ei = end, P = proof, A = assumption, C = result
 get_box(_,_,[],_,_) :- fail.
 get_box(Bi,Ei,[B|_],A,C) :- B = [[Bi,A,assumption]|_],reverse(B,[[Ei,C,_]|_]),!.
 get_box(Bi,Ei,[_|T],A,C) :- get_box(Bi,Ei,T,A,C).
 
-% kolla om element finns i lista ( därför att member förstör tester.. )
+% check if element is in list ( because member ruins testing.. )
 in_list(_,[]) :- fail.
 in_list(X,[H|_]) :- X = H,!.
 in_list(X,[_|T]) :- in_list(X,T).
